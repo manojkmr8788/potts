252411fd04187cfde4de9a7844fe895e81804afd
Manoj26 <64610293+manojkmr8788@users.noreply.github.com>
2022-10-29 00:34:58 +0200

    Update fit_script.sh

--- fit_script.sh
+++ fit_script.sh
@@ -5,11 +5,12 @@
 
 gnuplot -persist << EOF
 
+## Joint fit function f(x,y), where exponents b and d have a common value. The parameters e0 and e1 are the extrapolate estimates for m and e_J, respectively.  
 
-f(x,y)=value(sprintf("a%d",y))*x**-b*(1+value(sprintf("c%d",y))*x**-d)+value(sprintf("e%d",y))  ## Joint fit function f(x,y), where exponents b and d have a common value. The parameters e0 and e1 are 												    ## the extrapolate estimates for m and e_J, respectively.  
-
+f(x,y)=value(sprintf("a%d",y))*x**-b*(1+value(sprintf("c%d",y))*x**-d)+value(sprintf("e%d",y)) 
 fit f(x,y) 'inputFile.dat' u 1:-1:2:3 via a0,a1,b,c0,c1,d,e0,e1
-set print "outputFile.dat" append    								    ##create an output file
-print b,b_err/FIT_STDFIT,d,d_err/FIT_STDFIT,e0,e0_err/FIT_STDFIT,e1,e1_err/FIT_STDFIT,FIT_P     ##store the power-law exponents and extrapolated results alongwith fit quality FIT_P in the output file 
+
+set print "outputFile.dat" append    								                                         ##create an output file
+print b,b_err/FIT_STDFIT,d,d_err/FIT_STDFIT,e0,e0_err/FIT_STDFIT,e1,e1_err/FIT_STDFIT,FIT_P   ##store the power-law exponents and extrapolated results alongwith fit quality FIT_P in the output file 
 
 EOF
